5fa8de390c047ec7b60460983b70d185813651c5
Jenny Liu Chen <64031304+JennyChen9827@users.noreply.github.com>
2025-10-03 15:55:13 -0400

    Update main.c

--- main.c
+++ main.c
@@ -8,44 +8,11 @@ int sum_array(int arr[], int n){
 
 
 void count_characters() {
-
-    
-    FILE *inFile = fopen("input.txt", "r");
-    FILE *outFile = fopen("output.txt", "w");
-
-    if (inFile == NULL || outFile == NULL) {
-        printf("Error opening file.\n");
-        if (inFile) fclose(inFile);
-        if (outFile) fclose(outFile);
-        return;
-    }
-
-    int letters = 0, digits = 0, special = 0;
-    char ch;
-
-    while ((ch = fgetc(inFile)) != EOF) {
-        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-            letters++;
-        } else if (ch >= '0' && ch <= '9') {
-            digits++;
-        } else if (ch != ' ' && ch != '\n') {
-            special++;
-        }
-    }
-
-    fprintf(outFile, "Letters: %d\n", letters);
-    
-    fprintf(outFile, "Digits: %d\n", digits);
-    fprintf(outFile, "Special Characters: %d\n", special);
-    
-    printf( "Letters: %d\n", letters);
-    printf( "Digits: %d\n", digits);
-    printf( "Special Characters: %d\n", special);
-
-    fclose(inFile);
-    fclose(outFile);
+// implement
+  /*  
+     */
 }
-
+// don't edit this
 int main() {
     
     int function;
